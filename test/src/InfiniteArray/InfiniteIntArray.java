38432da8ba5be364f3e093bc4fc6c6f4f52de5ce
ghkden55 <ghkden55@gmail.com>
2024-01-25 17:45:44 +0900

    [Update] Infinite Int Array
    - 특정 인덱스의 원소 값을 바꾸는 메서드 생성: replace 메서드
    
     - 특정 인덱스의 원소 값을 제거하는 메서드 생성: remove 메서드
    
    - 특정 인덱스의 원소 값을 반환하는 메서드 생성: getElement 메서드

--- test/src/InfiniteArray/InfiniteIntArray.java
+++ test/src/InfiniteArray/InfiniteIntArray.java
@@ -25,9 +25,45 @@ public class InfiniteIntArray {
         }
     }
 
+
+    public void replace(int index, int value) {
+
+        try {
+            arr[index] = value;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            if (index > idx) {
+                int[] temp = new int[index + 1];
+
+                for (int i = 0 ; i < arr.length ; i++) {
+                    temp[i] = arr[i];
+                }
+
+                arr = temp;
+
+                arr[index] = value;
+            }
+        }
+    }
+
+
+    public void remove(int index) {
+        for (int i = index ; i < arr.length - 1 ; i++) {
+            arr[i] = arr[i + 1];
+        }
+        idx--;
+    }
+
+
+    public int getElement(int index) {
+        return arr[index];
+    }
+
+
     public void printArray() {
         System.out.println(Arrays.toString(arr));
     }
+
 
     public void printIndexArray(int index) {
         System.out.println(arr[index]);
