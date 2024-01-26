4d1881777c0814e2f83ef3f8a9342a53c7f0329f
ghkden55 <ghkden55@gmail.com>
2024-01-26 17:58:41 +0900

    [Update] Update Student Class
    - Getter, Setter 추가
    - 생성자 삭제

--- StudentGradesProcessingProgram/src/Student.java
+++ StudentGradesProcessingProgram/src/Student.java
@@ -5,12 +5,39 @@ public class Student {
     int age;
     String gender;
 
-    public Student(int sno, String name, int age, String gender) {
+
+    public int getSno() {
+        return sno;
+    }
+
+    public void setSno(int sno) {
         this.sno = sno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
         this.gender = gender;
     }
+
 
     @Override
     public String toString() {
